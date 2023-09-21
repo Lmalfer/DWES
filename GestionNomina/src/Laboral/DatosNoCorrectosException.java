d0ca3d58509855548c6dad3c5c9c8200bb5da0cf
Lmalfer <122611053+Lmalfer@users.noreply.github.com>
2023-09-21 13:48:28 +0200

    Delete GestionNomina directory

--- GestionNomina/src/Laboral/DatosNoCorrectosException.java
+++
@@ -1,7 +0,0 @@
-package Laboral;
-
-public class DatosNoCorrectosException extends Exception {
-    public DatosNoCorrectosException(String e) {
-        System.out.println("Los datos no son correctos");
-    }
-}
